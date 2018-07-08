04f84a9fca562b0c1deffe6dc7c5c006297dc7e9
moshe hanuka <mosheh77@gmail.com>
2018-07-09 00:18:46 +0300

    implement message directive - only template

---
+++ app/src/directives/message/message.js
@@ -0,0 +1,13 @@
+class MessageCtrl {
+
+    constructor() {
+    }
+}
+
+angular.module('mail-app').component('message',  {
+  controller: MessageCtrl,
+  templateUrl: 'src/directives/message/message.html',
+  bindings: {
+    message: '=',
+  },
+});
